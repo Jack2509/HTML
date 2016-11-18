dd3a3daa27c16e535d072048bd25f7fe1644955f
Nguyen Cong Dinh <dinh.nguyen@sutrixsolutions.com>
2016-11-18 15:46:09 +0700

    commit code

---
+++ js/plugins/slider-banner.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $('#home_banner').slick({
+    //autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 2000,
+    infinite: true,
+    slidesPerRow: 1,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true
+  });
+});
